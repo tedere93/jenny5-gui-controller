6430fb08ab0e0ab77c2fe0c39395c9ede1753f5c
Mihai Oltean <mihai.oltean@gmail.com>
2017-01-02 20:48:34 +0200

    added sliders and textctrl for motor position of the head

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -17,6 +17,7 @@
 #include "head_face_follow.h"
 #include "lidar_map.h"
 
+
 MainFrame *f_main;
 
 //#include <vld.h>
@@ -36,78 +37,120 @@ void MainFrame::BuildInterface(void)
 	wxPanel *p_log_panel = new wxPanel(splitter_commands_log, wxID_ANY, wxDefaultPosition, wxSize(-1, 200));
 
 
-	p_com_ports = new wxPanel(p_commands_panel);
 	p_predefined_tasks = new wxPanel(p_commands_panel);
-	p_free_moves = new wxPanel(p_commands_panel);
-
-	wxBoxSizer* sizer_commands = new wxBoxSizer(wxHORIZONTAL);
-	sizer_commands->Add(p_com_ports);
-	sizer_commands->Add(p_predefined_tasks, 0, wxLEFT, 10);
-	sizer_commands->Add(p_free_moves, 0, wxLEFT, 10);
-
-	p_commands_panel->SetSizer(sizer_commands);
+	p_head = new wxPanel(p_commands_panel);
+	p_left_arm = new wxPanel(p_commands_panel);
+	p_right_arm = new wxPanel(p_commands_panel);
+	p_lidar = new wxPanel(p_commands_panel);
+	p_platform = new wxPanel(p_commands_panel);
 
 	wxBoxSizer* sizer_predefined_tasks = new wxBoxSizer(wxVERTICAL);
-
+	st_predefined_tasks = new wxStaticText(p_predefined_tasks, -1, "Predefined tasks");
 	b_head_face_following = new wxButton(p_predefined_tasks, wxID_ANY, "Head follow face");
 	b_head_face_following->Bind(wxEVT_BUTTON, &MainFrame::on_head_face_follow_click, this);
-
 	b_lidar_map = new wxButton(p_predefined_tasks, wxID_ANY, "LIDAR map");
 	b_lidar_map->Bind(wxEVT_BUTTON, &MainFrame::on_lidar_map_click, this);
 	
-	sizer_predefined_tasks->Add(b_head_face_following);
-	sizer_predefined_tasks->Add(b_lidar_map);
+	sizer_predefined_tasks->Add(st_predefined_tasks, 0, wxTOP, 10);
+	sizer_predefined_tasks->Add(b_head_face_following, 0, wxTOP, 10);
+	sizer_predefined_tasks->Add(b_lidar_map, 0, wxTOP, 10);
 
 	p_predefined_tasks->SetSizer(sizer_predefined_tasks);
 
-	wxBoxSizer* sizer_com_ports = new wxBoxSizer(wxVERTICAL);
-
-	st_head_com_port = new wxStaticText(p_com_ports, wxID_ANY, "Head COM port");
-	tc_head_com_port = new wxTextCtrl(p_com_ports, wxID_ANY, "9");
-	b_connect_to_head = new wxButton(p_com_ports, -1, "Connect to head");
+	wxBoxSizer* sizer_head = new wxBoxSizer(wxVERTICAL);
+	st_head = new wxStaticText(p_head, wxID_ANY, "Head");
+	st_head_com_port = new wxStaticText(p_head, wxID_ANY, "COM port");
+	tc_head_com_port = new wxTextCtrl(p_head, wxID_ANY, "9");
+	b_connect_to_head = new wxButton(p_head, -1, "Connect");
 	b_connect_to_head->Bind(wxEVT_BUTTON, &MainFrame::on_connect_to_head_click, this);
 
-	st_left_arm_com_port = new wxStaticText(p_com_ports, wxID_ANY, "Left arm COM port");
-	tc_left_arm_com_port = new wxTextCtrl(p_com_ports, wxID_ANY, "1");
-	b_connect_to_left_arm = new wxButton(p_com_ports, -1, "Connect to left");
+	st_head_H_motor_position = new wxStaticText(p_head, wxID_ANY, "Horizontal motor");
+	tc_head_H_motor_position = new wxTextCtrl(p_head, wxID_ANY, "0");
+	s_head_H_motor_position = new wxSlider(p_head, -1, 0, 0, _head_horizontal_motor_potentiometer_max - _head_horizontal_motor_potentiometer_min);
+	st_head_V_motor_position = new wxStaticText(p_head, wxID_ANY, "Vertical motor");
+	tc_head_V_motor_position = new wxTextCtrl(p_head, wxID_ANY, "0");
+	s_head_V_motor_position = new wxSlider(p_head, -1, 0, 0, _head_vertical_motor_potentiometer_max - _head_vertical_motor_potentiometer_min);
+	b_head_refresh = new wxButton(p_head, -1, "Refresh");
+	st_head_ultrasonic = new wxStaticText(p_head, wxID_ANY, "Ultrasonic");
+	tc_head_ultrasonic = new wxTextCtrl(p_head, wxID_ANY, "0");
+	tc_head_ultrasonic->SetEditable(false);
+	b_head_refresh->Bind(wxEVT_BUTTON, &MainFrame::on_refresh_head_click, this);
+
+	sizer_head->Add(st_head, 0, wxTOP, 10);
+	sizer_head->Add(st_head_com_port, 0, wxTOP, 10);
+	sizer_head->Add(tc_head_com_port);
+	sizer_head->Add(b_connect_to_head, 0, wxTOP, 10);
+	sizer_head->Add(st_head_H_motor_position, 0, wxTOP, 10);
+	sizer_head->Add(tc_head_H_motor_position);
+	sizer_head->Add(s_head_H_motor_position);
+	sizer_head->Add(st_head_V_motor_position, 0, wxTOP, 10);
+	sizer_head->Add(tc_head_V_motor_position);
+	sizer_head->Add(s_head_V_motor_position);
+	sizer_head->Add(st_head_ultrasonic, 0, wxTOP, 10);
+	sizer_head->Add(tc_head_ultrasonic);
+	sizer_head->Add(b_head_refresh, 0, wxTOP, 10);
+
+	p_head->SetSizer(sizer_head);
+
+	wxBoxSizer* sizer_left_arm = new wxBoxSizer(wxVERTICAL);
+	st_left_arm = new wxStaticText(p_left_arm, wxID_ANY, "Left arm");
+	st_left_arm_com_port = new wxStaticText(p_left_arm, wxID_ANY, "COM port");
+	tc_left_arm_com_port = new wxTextCtrl(p_left_arm, wxID_ANY, "1");
+	b_connect_to_left_arm = new wxButton(p_left_arm, -1, "Connect");
 	b_connect_to_left_arm->Bind(wxEVT_BUTTON, &MainFrame::on_connect_to_left_arm_click, this);
+	sizer_left_arm->Add(st_left_arm, 0, wxTOP, 10);
+	sizer_left_arm->Add(st_left_arm_com_port, 0, wxTOP, 10);
+	sizer_left_arm->Add(tc_left_arm_com_port);
+	sizer_left_arm->Add(b_connect_to_left_arm, 0, wxTOP, 10);
+	p_left_arm->SetSizer(sizer_left_arm);
 
-	st_right_arm_com_port = new wxStaticText(p_com_ports, wxID_ANY, "Right COM port");
-	tc_right_arm_com_port = new wxTextCtrl(p_com_ports, wxID_ANY, "2");
-	b_connect_to_right_arm = new wxButton(p_com_ports, -1, "Connect to right");
+	wxBoxSizer* sizer_right_arm = new wxBoxSizer(wxVERTICAL);
+	st_right_arm = new wxStaticText(p_right_arm, wxID_ANY, "Right arm");
+	st_right_arm_com_port = new wxStaticText(p_right_arm, wxID_ANY, "COM port");
+	tc_right_arm_com_port = new wxTextCtrl(p_right_arm, wxID_ANY, "2");
+	b_connect_to_right_arm = new wxButton(p_right_arm, -1, "Connect");
 	b_connect_to_right_arm->Bind(wxEVT_BUTTON, &MainFrame::on_connect_to_right_arm_click, this);
+	sizer_right_arm->Add(st_right_arm, 0, wxTOP, 10);
+	sizer_right_arm->Add(st_right_arm_com_port, 0, wxTOP, 10);
+	sizer_right_arm->Add(tc_right_arm_com_port);
+	sizer_right_arm->Add(b_connect_to_right_arm, 0, wxTOP, 10);
+	p_right_arm->SetSizer(sizer_right_arm);
 
-	st_lidar_com_port = new wxStaticText(p_com_ports, wxID_ANY, "Lidar COM port");
-	tc_lidar_com_port = new wxTextCtrl(p_com_ports, wxID_ANY, "8");
-	b_connect_to_lidar = new wxButton(p_com_ports, -1, "Connect to lidar");
+	wxBoxSizer* sizer_lidar = new wxBoxSizer(wxVERTICAL);
+	st_lidar = new wxStaticText(p_lidar, wxID_ANY, "LIDAR");
+	st_lidar_com_port = new wxStaticText(p_lidar, wxID_ANY, "COM port");
+	tc_lidar_com_port = new wxTextCtrl(p_lidar, wxID_ANY, "8");
+	b_connect_to_lidar = new wxButton(p_lidar, -1, "Connect");
 	b_connect_to_lidar->Bind(wxEVT_BUTTON, &MainFrame::on_connect_to_lidar_click, this);
+	sizer_lidar->Add(st_lidar, 0, wxTOP, 10);
+	sizer_lidar->Add(st_lidar_com_port, 0, wxTOP, 10);
+	sizer_lidar->Add(tc_lidar_com_port);
+	sizer_lidar->Add(b_connect_to_lidar, 0, wxTOP, 10);
+	p_lidar->SetSizer(sizer_lidar);
 
-	st_platform_com_port = new wxStaticText(p_com_ports, wxID_ANY, "Platform COM port");
-	tc_platform_com_port = new wxTextCtrl(p_com_ports, wxID_ANY, "4");
-	b_connect_to_platform = new wxButton(p_com_ports, -1, "Connect to platform");
+	wxBoxSizer* sizer_platform = new wxBoxSizer(wxVERTICAL);
+	st_platform = new wxStaticText(p_platform, wxID_ANY, "Platform");
+	st_platform_com_port = new wxStaticText(p_platform, wxID_ANY, "COM port");
+	tc_platform_com_port = new wxTextCtrl(p_platform, wxID_ANY, "4");
+	b_connect_to_platform = new wxButton(p_platform, -1, "Connect");
 	b_connect_to_platform->Bind(wxEVT_BUTTON, &MainFrame::on_connect_to_platform_click, this);
+	sizer_platform->Add(st_platform, 0, wxTOP, 10);
+	sizer_platform->Add(st_platform_com_port, 0, wxTOP, 10);
+	sizer_platform->Add(tc_platform_com_port);
+	sizer_platform->Add(b_connect_to_platform, 0, wxTOP, 10);
+	p_platform->SetSizer(sizer_platform);
 
-	sizer_com_ports->Add(st_head_com_port, 0, wxTOP, 10);
-	sizer_com_ports->Add(tc_head_com_port);
-	sizer_com_ports->Add(b_connect_to_head);
+	wxBoxSizer* sizer_commands = new wxBoxSizer(wxHORIZONTAL);
 
-	sizer_com_ports->Add(st_left_arm_com_port, 0, wxTOP, 10);
-	sizer_com_ports->Add(tc_left_arm_com_port);
-	sizer_com_ports->Add(b_connect_to_left_arm);
-	
-	sizer_com_ports->Add(st_right_arm_com_port, 0, wxTOP, 10);
-	sizer_com_ports->Add(tc_right_arm_com_port);
-	sizer_com_ports->Add(b_connect_to_right_arm);
-	
-	sizer_com_ports->Add(st_lidar_com_port, 0, wxTOP, 10);
-	sizer_com_ports->Add(tc_lidar_com_port);
-	sizer_com_ports->Add(b_connect_to_lidar);
-	
-	sizer_com_ports->Add(st_platform_com_port, 0, wxTOP, 10);
-	sizer_com_ports->Add(tc_platform_com_port);
-	sizer_com_ports->Add(b_connect_to_platform);
+	sizer_commands->Add(p_predefined_tasks, 0, wxLEFT, 10);
+	sizer_commands->Add(p_head, 0, wxLEFT, 10);
+	sizer_commands->Add(p_lidar, 0, wxLEFT, 10);
+	sizer_commands->Add(p_platform, 0, wxLEFT, 10);
+	sizer_commands->Add(p_left_arm, 0, wxLEFT, 10);
+	sizer_commands->Add(p_right_arm, 0, wxLEFT, 10);
 
-	p_com_ports->SetSizer(sizer_com_ports);
+	p_commands_panel->SetSizer(sizer_commands);
+
 
 	tc_log = new wxTextCtrl(p_log_panel, -1, "Jenny 5 controller log\n", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
 	wxBoxSizer* sizer_log = new wxBoxSizer(wxVERTICAL);
@@ -140,7 +183,14 @@ MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& si
 //------------------------------------------------------------------------
 void MainFrame::OnMainWindowClose(wxCloseEvent& WXUNUSED(event))
 {
+	t_timer->Stop();
 	delete t_timer;
+
+	head_controller.close_connection();
+	left_arm_controller.close_connection();
+	right_arm_controller.close_connection();
+	lidar_controller.close_connection();
+	platform_controller.close_connection();
 
 	Destroy();
 }
@@ -177,7 +227,7 @@ void  MainFrame::on_connect_to_head_click(wxCommandEvent &event)
 
 	if (!head_controller.is_open()) {
 		if (connect_to_head(head_controller, head_cam, head_com_port, error_string)) {
-			b_connect_to_head->SetLabel("Disconnect head");
+			b_connect_to_head->SetLabel("Disconnect");
 			// show the firmware version number
 			head_controller.send_get_firmware_version();
 
@@ -187,6 +237,7 @@ void  MainFrame::on_connect_to_head_click(wxCommandEvent &event)
 			else {
 				head_controller.send_get_potentiometer_position(0);
 				head_controller.send_get_potentiometer_position(1);
+				head_controller.send_get_sonar_distance(0);
 			}
 		}
 		else {
@@ -194,8 +245,9 @@ void  MainFrame::on_connect_to_head_click(wxCommandEvent &event)
 		}
 	}// is open, so just disconect
 	else {
+		write_to_log("Disconnected from head");
 		head_controller.close_connection();
-		b_connect_to_head->SetLabel("Connect head");
+		b_connect_to_head->SetLabel("Connect");
 	}
 }
 //------------------------------------------------------------------------
@@ -235,13 +287,31 @@ void MainFrame::on_timer(wxTimerEvent& event)
 		int pot_position;
 		char buffer[100];
 		if (head_controller.query_for_event(POTENTIOMETER_EVENT, HEAD_POTENTIOMETER_HORIZONTAL_INDEX, &pot_position)) {
-			sprintf(buffer, "Pot position (%d) = %d\n", HEAD_POTENTIOMETER_HORIZONTAL_INDEX, pot_position);
+			sprintf(buffer, "Head pot position (%d) = %d\n", HEAD_POTENTIOMETER_HORIZONTAL_INDEX, pot_position);
 			write_to_log(buffer);
+			tc_head_H_motor_position->SetValue(wxString() << pot_position);
 		}
 		if (head_controller.query_for_event(POTENTIOMETER_EVENT, HEAD_POTENTIOMETER_VERTICAL_INDEX, &pot_position)) {
-			sprintf(buffer, "Pot position (%d) = %d\n", HEAD_POTENTIOMETER_VERTICAL_INDEX, pot_position);
+			sprintf(buffer, "Head pot position (%d) = %d\n", HEAD_POTENTIOMETER_VERTICAL_INDEX, pot_position);
 			write_to_log(buffer);
+			tc_head_V_motor_position->SetValue(wxString() << pot_position);
 		}
+
+		int ultrasonic_distance;
+		if (head_controller.query_for_event(SONAR_EVENT, 0, &ultrasonic_distance)) {
+			sprintf(buffer, "Head ultrasonic distance = %d\n", ultrasonic_distance);
+			write_to_log(buffer);
+			tc_head_ultrasonic->SetValue(wxString() << ultrasonic_distance);
+		}
+		
+
 	}
+}
+//------------------------------------------------------------------------
+void MainFrame::on_refresh_head_click(wxCommandEvent &event)
+{
+	head_controller.send_get_potentiometer_position(0);
+	head_controller.send_get_potentiometer_position(1);
+	head_controller.send_get_sonar_distance(0);
 }
 //------------------------------------------------------------------------
--- src/main_window.h
+++ src/main_window.h
@@ -28,6 +28,7 @@
 #include <opencv2\imgproc\imgproc.hpp>
 
 #include "jenny5_arduino_controller.h"
+#include "roboclaw_controller.h"
 //------------------------------------------------------------------------
 class MainFrame : public wxFrame
 {
@@ -36,37 +37,55 @@ private:
 	wxSplitterWindow * splitter_commands_log;
 
 	wxPanel *p_predefined_tasks;
-	wxPanel *p_com_ports;
-	wxPanel *p_free_moves;
+	wxPanel *p_head;
+	wxPanel *p_left_arm;
+	wxPanel *p_right_arm;
+	wxPanel *p_lidar;
+	wxPanel *p_platform;
 
+	wxStaticText *st_head;
 	wxStaticText *st_head_com_port;
 	wxTextCtrl *tc_head_com_port;
 	wxButton *b_connect_to_head;
+	wxStaticText *st_head_H_motor_position;
+	wxTextCtrl *tc_head_H_motor_position;
+	wxSlider *s_head_H_motor_position;
+	wxStaticText *st_head_V_motor_position;
+	wxTextCtrl *tc_head_V_motor_position;
+	wxSlider *s_head_V_motor_position;
+	wxStaticText *st_head_ultrasonic;
+	wxTextCtrl *tc_head_ultrasonic;
+	wxButton *b_head_refresh;
 
+	wxStaticText *st_left_arm;
 	wxStaticText *st_left_arm_com_port;
 	wxTextCtrl *tc_left_arm_com_port;
 	wxButton *b_connect_to_left_arm;
 
+	wxStaticText *st_right_arm;
 	wxStaticText *st_right_arm_com_port;
 	wxTextCtrl *tc_right_arm_com_port;
 	wxButton *b_connect_to_right_arm;
 
+	wxStaticText *st_lidar;
 	wxStaticText *st_lidar_com_port;
 	wxTextCtrl *tc_lidar_com_port;
 	wxButton *b_connect_to_lidar;
 
+	wxStaticText *st_platform;
 	wxStaticText *st_platform_com_port;
 	wxTextCtrl *tc_platform_com_port;
 	wxButton *b_connect_to_platform;
 
+	wxStaticText *st_predefined_tasks;
 	wxButton *b_head_face_following;
 	wxButton *b_lidar_map;
 
-	
 	t_jenny5_arduino_controller head_controller;
 	t_jenny5_arduino_controller left_arm_controller;
 	t_jenny5_arduino_controller right_arm_controller;
 	t_jenny5_arduino_controller lidar_controller;
+	t_roboclaw_controller platform_controller;
 
 	cv::VideoCapture head_cam;
 	cv::VideoCapture left_arm_cam;
@@ -74,13 +93,15 @@ private:
 
 	cv::CascadeClassifier face_classifier;
 
-
 	void BuildInterface(void);
 	void OnMainWindowClose(wxCloseEvent& WXUNUSED(event));
 
 	void on_head_face_follow_click(wxCommandEvent &event);
 	void on_lidar_map_click(wxCommandEvent &event);
+	
 	void on_connect_to_head_click(wxCommandEvent &event);
+	void on_refresh_head_click(wxCommandEvent &event);
+
 	void on_connect_to_lidar_click(wxCommandEvent &event);
 	void on_connect_to_left_arm_click(wxCommandEvent &event);
 	void on_connect_to_right_arm_click(wxCommandEvent &event);
